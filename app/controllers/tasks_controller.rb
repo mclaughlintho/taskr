52f380af06077705a3d914f77bd363b5c615d38e
Tom <mclaughlin.tho@gmail.com>
2015-06-06 07:23:20 +0000

    begun tasks restfulness

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,29 @@
+class TasksController < ApplicationController
+  
+  def index
+    @tasks = Task.all
+    @task = Task.new
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  def create
+    @task = Task.create(task_params)
+    redirect_to @task
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    @task.update_attributes(task_params)
+    redirect_to @task
+  end
+  
+  private
+  
+  def task_params
+    params.require(:task).permit(:title, :complete)
+  end
+    
+end
